ea7ef3ff9324cf0165a14bbae84b2499aee246df
Anthony Petrangelo <tonypetrangelo@gmail.com>
2020-03-24 14:57:40 -0500

    changed pdf display

--- src/containers/Source.js
+++ src/containers/Source.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button, Badge } from 'react-bootstrap';
 import { apiHeader } from "../libs/api";
 import SidebarPost from "../components/SidebarPost";
-import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 //import "./Home.css";
 
 export default function Source(props) {
@@ -13,7 +11,7 @@ export default function Source(props) {
   const [numPages, setNumPages] = useState(0);
 
   const queryStr = `source?slug=${props.match.params.source}`;
-
+  //'https://cdn.' + source[0].attachment.guid.substring(source[0].attachment.guid.search('api.')+4)
   useEffect(() => {
     async function onLoad() {
       // get page content
@@ -41,26 +39,36 @@ export default function Source(props) {
             </Col>
           </Row>
           <Row>
-            <Col md={1}></Col>
-            <Col md={7}>
-              <div dangerouslySetInnerHTML={{ __html: source[0].content.rendered }} />
+            <Col lg={1}></Col>
+            <Col lg={7}>
+              <Row className="mb-2">
+                <Col>
+                  <div className="html-content" dangerouslySetInnerHTML={{ __html: source[0].content.rendered }} />
+                </Col>
+              </Row>
               {source[0].attachment
-                ? <div>
-                    <Document
-                      file={'https://cdn.' + source[0].attachment.guid.substring(source[0].attachment.guid.search('api.')+4)}
-                      onLoadSuccess={setNumPages}
-                    >
-                      <Page pageNumber={pageNumber} />
-                    </Document>
-                    <p>Page {pageNumber} of {numPages}</p>
-                  </div>
+                ? <Row className="my-4">
+                    <Col>
+                      <div className="mb-2">
+                        <object width="100%" height="950px" data={'https://cdn.' + source[0].attachment.guid.substring(source[0].attachment.guid.search('api.')+4)} type="application/pdf"> 
+                          <p>It appears you don't have a PDF plugin for this browser.
+                           No biggie... you can <a href="resume.pdf">click here to
+                          download the PDF file.</a></p>  
+                        </object>
+                      </div>
+                      <div className="text-center">
+                        <a href={'https://cdn.' + source[0].attachment.guid.substring(source[0].attachment.guid.search('api.')+4)}>
+                          link to pdf file</a>
+                      </div>
+                    </Col>
+                  </Row>
                 : null
               }
             </Col>
-            <Col md={3}>
+            <Col lg={3}>
               {source[0].source_person
                 ? <div><div className="p-3 mb-3 bg-light rounded">
-                    <h4 className="font-italic">Author</h4>
+                    <h4 className="font-italic">Related Author</h4>
                   </div>
                   <SidebarPost
                     title = {source[0].source_person[0].post_title}
@@ -72,7 +80,7 @@ export default function Source(props) {
               }
               {source[0].source_theory
                 ? <div><div className="p-3 mb-3 bg-light rounded">
-                    <h4 className="font-italic">Theory</h4>
+                    <h4 className="font-italic">Related Theory</h4>
                   </div><SidebarPost
                     title = {source[0].source_theory[0].post_title}
                     text = {source[0].source_theory[0].post_excerpt}
@@ -82,7 +90,7 @@ export default function Source(props) {
                 : null
               }
             </Col>
-            <Col md={1}></Col>
+            <Col lg={1}></Col>
           </Row></div>
         : null
       }
