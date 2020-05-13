b89d2d1012ddff193187919ddff47786b3dbc277
Anthony Petrangelo <tonypetrangelo@gmail.com>
2020-05-13 07:01:20 -0500

    added tags and cats to blog page

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,36 +1,69 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Badge } from 'react-bootstrap';
 import { apiHeader } from "../libs/api";
 import StandardHelmet from "../components/StandardHelmet";
 import PageTitle from "../components/PageTitle";
 import SummaryPost from "../components/SummaryPost";
+import SidebarSection from "../components/SidebarSection";
+import SidebarTitle from "../components/SidebarTitle";
+import { categoryBadge, categoryNames, categoryIDs } from "../libs/categories";
 
 //import "./Home.css";
 
 export default function Blog(props) {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
+  const [sources, setSources] = useState(false);
+  const [tags, setTags] = useState([]);
   //const [pages, setPages] = useState(10);
 
   const pages = 100;
   let blogQuery = `?page=${page}&per_page=${pages}&_fields=categories,title,date,excerpt,slug,sticky`;
+  let sourceStr = `source?_fields=title,excerpt,slug,date&include=99,98`;
+  let tagStr = `tags?_fields=count,name,slug&page=1&per_page=100&hide_empty=true`;
 
   useEffect(() => {
     async function onLoad() {
+      let payload = [];
+      let response = "";
       props.setIsLoaded(false);
       // get posts
       try {
-        const response = await fetch(apiHeader + "posts" + blogQuery);
+        response = await fetch(apiHeader + "posts" + blogQuery);
         if (response.ok) { // ckeck if status code is 200
-          const payload = await response.json();
-          console.log(payload);
+          payload = await response.json();
+          //console.log(payload);
           setPosts(payload);
         } 
       } catch (e) {
         alert(e);
       }
       props.setIsLoaded(true);
+
+      // get sources
+      try {
+        response = await fetch(apiHeader + sourceStr);
+        if (response.ok) { // ckeck if status code is 200
+          payload = await response.json();
+          //console.log(sourceIDs);
+          setSources(payload);
+        } 
+      } catch (e) {
+        alert(e);
+      }
+
+      // get tags
+      try {
+        response = await fetch(apiHeader + tagStr);
+        if (response.ok) { // ckeck if status code is 200
+          payload = await response.json();
+          console.log(payload);
+          setTags(payload);
+        } 
+      } catch (e) {
+        alert(e);
+      }
     }
 
     onLoad();
@@ -42,9 +75,8 @@ export default function Blog(props) {
         title={"Blog"}
         link={"https://atlantis.fyi/blog"} 
       />
-      <PageTitle loaded={props.isLoaded}>Dialogue</PageTitle>
+      <PageTitle loaded={props.isLoaded}>Blog Posts</PageTitle>
 	    <Row>
-        <Col className="d-none d-lg-block" lg={2}></Col>
         {posts.length > 0
 	        ? <Col lg={8} className="blog-main">
               {posts.map((post, index) => 
@@ -61,7 +93,25 @@ export default function Blog(props) {
     	      </Col>
           : null
         }
-        <Col className="d-none d-lg-block" lg={2}></Col>
+        <Col md={4}>
+          {sources
+            ? <SidebarSection
+                data = {sources}
+                titleSingle = "Atlantis dialogue"
+                titleMultiple = "Atlantis dialogues"
+                linkPath = "source"
+              />
+            : null
+          }
+          <SidebarTitle>Blog Categories</SidebarTitle>
+          <div className="mb-4">
+            {Object.keys(categoryIDs).map((item, index) => <Link key={index} to={"/blog/category/"+item}><Badge className="categoryLink m-2" variant={categoryBadge[categoryIDs[item]]}>{categoryNames[categoryIDs[item]]}</Badge></Link>)}
+          </div>
+          <SidebarTitle>Blog Tags</SidebarTitle>
+          <div>
+            {tags.map((item, index) => <Link key={index} to={"/blog/tag/"+item.slug}><Badge className="tagLink m-1" variant={"primary"}>{item.name}</Badge></Link>)}
+          </div>
+        </Col>
 	    </Row>
 	  </main>
   );
