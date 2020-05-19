bd47a26170cdfedbd96440cb5d72e7549320b9e4
Anthony Petrangelo <tonypetrangelo@gmail.com>
2020-05-19 12:31:46 -0500

    added sitemap

---
+++ sitemap-routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+export default (
+  <Route>
+    <Route path="/" />
+    <Route path="/blog" />
+    <Route path="/theories" />
+    <Route path="/sources" />
+    <Route path="/people"  />
+    <Route path="/plato" />
+    <Route path="/about" />
+  </Route>
+);
